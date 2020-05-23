9613c320c20ec1c3249a337112dda08c331bc958
nmkip <juan.ignacio848@gmail.com>
2020-05-23 16:29:11 -0300

    C-# toggle reader macro sexp comment. Evil normal state after toggle.

--- spacemacs.el
+++ spacemacs.el
@@ -606,6 +606,26 @@ before packages are loaded."
     :config
     (require 'flycheck-clj-kondo))
 
+  ;; toggle reader macro sexp comment
+  ;; toggles the #_ characters at the start of an expression
+  (defun clojure-toggle-reader-comment-sexp ()
+    (interactive)
+    (let* ((point-pos1 (point)))
+      (evil-insert-line 0)
+      (let* ((point-pos2 (point))
+             (cmtstr "#_")
+             (cmtstr-len (length cmtstr))
+             (line-start (buffer-substring-no-properties point-pos2 (+ point-pos2 cmtstr-len)))
+             (point-movement (if (string= cmtstr line-start) -2 2))
+             (ending-point-pos (+ point-pos1 point-movement 1)))
+        (if (string= cmtstr line-start)
+            (delete-char cmtstr-len)
+          (insert cmtstr))
+        (goto-char ending-point-pos)))
+    (evil-normal-state))
+
+  ;; Assign keybinding to the toggle-reader-comment-sexp function
+  (define-key global-map (kbd "C-#") 'clojure-toggle-reader-comment-sexp)
   )
 
 ;; Do not write anything past this comment. This is where Emacs will
