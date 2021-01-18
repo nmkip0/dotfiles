0c5aaede9657fdd3ea6c67236d09e563955c047f
Juan Ignacio <juan.ignacio848@gmail.com>
2021-01-18 12:27:48 -0300

    move inside visual lines with j and k

--- init.el
+++ init.el
@@ -268,9 +268,10 @@ Buffer Transient State
   (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
   (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
 
-  ;; Use visual line motions even outside of visual-line-mode buffers
-  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
-  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+  ;; Use visual line motions in visual-line-mode buffers
+  (general-def 'normal 'visual-line-mode-map
+    "j" 'evil-next-visual-line
+    "k" 'evil-previous-visual-line)
 
   (evil-set-initial-state 'messages-buffer-mode 'normal)
   (evil-set-initial-state 'dashboard-mode 'normal))
