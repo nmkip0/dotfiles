8d9dcdffdf9bf52bcfcee25c7a1666e011d5432c
Juan Ignacio <juan.ignacio848@gmail.com>
2021-01-18 12:28:24 -0300

    Remove unused block

--- init.el
+++ init.el
@@ -330,14 +330,13 @@ Buffer Transient State
 
 (use-package counsel
   :bind (("C-M-j" . 'counsel-switch-buffer)
-  ;;     ("M-x" . counsel-M-x)
-  ;;     ("C-x b" . counsel-switch-buffer)
-  ;;     ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
          ("C-r" . 'counsel-minibuffer-history))
   :config
   (setq ivy-initial-inputs-alist nil)
   (counsel-mode 1))
+
+
 
 (use-package helpful
   :custom
