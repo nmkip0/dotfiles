43c5d9ceeb54739940f35178c8d707cca494da68
Juan Ignacio <juan.ignacio848@gmail.com>
2021-01-20 13:44:03 -0300

    Toggle flycheck-mode keybiding.

--- init.el
+++ init.el
@@ -216,6 +216,7 @@
       "wv" '(split-window-right :which-key "Split vertically")
 
       ;; Toggles
+      "tf" '(flycheck-mode :whick-key "Flycheck mode")
       "tl" '(command-log-mode :whick-key "Command log mode")
       "tL" '(global-command-log-mode :whick-key "Command log mode")
       "ts" '(smartparens-mode :whick-key "Smartparens mode")
