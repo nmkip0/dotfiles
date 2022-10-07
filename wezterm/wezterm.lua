c14a7bd9593a56b5f971a5ce6471f5dbf29417c0
nmkip <juan.ignacio848@gmail.com>
2022-10-06 23:58:08 -0300

    wezterm: initial config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,13 @@
+local wezterm = require 'wezterm'
+
+return {
+  font_size = 10.5,
+  color_scheme = "DoomOne",
+  enable_tab_bar = false,
+  window_padding = {
+    left = 0,
+    right = 0,
+    top = 0,
+    bottom = 0
+  }
+}
