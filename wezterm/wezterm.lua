ace74b69f49b6c3a3dbb81edd1ece299434fee86
nmkip <juan.ignacio848@gmail.com>
2022-10-11 23:30:52 -0300

    wezterm set nord theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 
 return {
   font_size = 10.5,
-  color_scheme = "DoomOne",
+  color_scheme = "nord",
   enable_tab_bar = false,
   window_padding = {
     left = 0,
