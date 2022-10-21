856d896b01f2d65187687da277deb4683a228f46
nmkip <juan.ignacio848@gmail.com>
2022-10-20 21:02:23 -0300

    nvim: nord theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
+require "user.colorscheme"
 
 require "user.cmp"
 require "user.lsp" --when requiring dirs lua looks for init.lua
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -45,6 +45,9 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
 
+  -- themes
+  use 'shaunsingh/nord.nvim'
+
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
   use "hrsh7th/cmp-buffer" -- buffer completions
