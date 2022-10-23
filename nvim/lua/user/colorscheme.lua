a398ca55842816733622faa4170d26924e24a7e1
nmkip <juan.ignacio848@gmail.com>
2022-10-23 15:58:49 -0300

    nvim: configure theme

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -6,3 +6,17 @@ catch /^Vim\%((\a\+)\)\=:E185/
   set background=dark
 endtry
 ]]
+
+
+local status_ok, nord = pcall(require, "nord")
+if not status_ok then
+  return
+end
+
+vim.g.nord_contrast = true
+vim.g.nord_borders = true
+vim.g.nord_disable_background = false
+vim.g.nord_italic = false
+vim.g.nord_uniform_diff_background = true
+
+nord.set()
