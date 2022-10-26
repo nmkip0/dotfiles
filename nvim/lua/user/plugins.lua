4055e424c9281cdc5a6284c3b792cb7142c1df6b
nmkip <juan.ignacio848@gmail.com>
2022-10-26 20:11:38 -0300

    nvim: add neodev

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -97,6 +97,7 @@ return packer.startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
   use "williamboman/nvim-lsp-installer"  -- simple to use language server installer
+  use "folke/neodev.nvim"
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
