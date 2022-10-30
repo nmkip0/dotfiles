ae04fa68917eba2bf4f945bc31bfb0d10e40a294
nmkip <juan.ignacio848@gmail.com>
2022-10-29 21:19:22 -0300

    nvim: workaround for folding

--- nvim/lua/user/treesitter.lua
+++ nvim/lua/user/treesitter.lua
@@ -6,16 +6,14 @@ end
 -- vim.opt.foldmethod     = 'expr'
 -- vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
 ---WORKAROUND
- --[[ vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, { ]]
- --[[   group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}), ]]
- --[[   callback = function() ]]
- --[[     vim.opt.foldmethod     = 'expr' ]]
- --[[     vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()' ]]
- --[[   end ]]
- --[[ }) ]]
+ vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+   group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+   callback = function()
+     vim.opt.foldmethod     = 'expr'
+     vim.opt.foldexpr       = 'nvim_treesitter#foldexpr()'
+   end
+ })
 ---ENDWORKAROUND
-vim.opt.foldmethod= "expr"
-vim.opt.foldexpr="nvim_treesitter#foldexpr()"
 
 configs.setup({
 	ensure_installed = "all", -- one of "all" or a list of languages
