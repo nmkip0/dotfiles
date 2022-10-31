7bac389c444062e9c5a0d26cdb8884ef432c590d
nmkip <juan.ignacio848@gmail.com>
2022-10-31 09:08:49 -0300

    nvim: disable diagnostics in conjure log and disable log by default

--- nvim/lua/user/conjure.lua
+++ nvim/lua/user/conjure.lua
@@ -4,7 +4,16 @@ vim.g["conjure#highlight#enabled"] = true
 vim.g["conjure#highlight#timeout"] = 150
 vim.g["conjure#log#wrap"] = true
 vim.g["conjure#log#jump_to_latest#enabled"] = true
+vim.g["conjure#log#hud#enabled"] = false
 vim.g["conjure#client#clojure#nrepl#eval#raw_out"] = true
 vim.g["conjure#client#clojure#nrepl#test#raw_out"] = true
 vim.g["conjure#client#clojure#nrepl#test#runner"] = "kaocha"
-vim.g["conjure#log#jump_to_latest#cursor_scroll_position"] = "bottom"
+vim.g["conjure#log#jump_to_latest#cursor_scroll_position"] = "center"
+
+vim.api.nvim_create_autocmd("BufNewFile", {
+  group = vim.api.nvim_create_augroup("CONJURE_LOG_DISABLE_LSP", {clear = true}),
+  pattern = "conjure-log-*",
+  callback = function()
+    vim.diagnostic.disable(0)
+  end
+})
