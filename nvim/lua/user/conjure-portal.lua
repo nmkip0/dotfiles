395e59efbb4b89efac06eb2d57d719a515b2c3a4
nmkip <juan.ignacio848@gmail.com>
2022-11-06 22:29:20 -0300

    nvim: portal commands

--- nvim/lua/user/conjure-portal.lua
+++ nvim/lua/user/conjure-portal.lua
@@ -26,20 +26,19 @@ local function conjure_form(is_root)
   return extract.form({ ["root?"] = is_root }).content
 end
 
-local portal_cmds = {
-  open = conjure_eval_fn [[
-    (def portal 
-      ((requiring-resolve 'portal.api/open)
-        {:theme :portal.colors/nord})) 
-    ]],
+local function invoke_portal_command(command)
+  return conjure_eval_fn(
+    [[
+    (do
+      (#?(:clj portal.api/eval-str :cljs portal.web/eval-str) "]]
+      .. command ..
+    [[
+      ")
+    )]]
+  )
+end
 
-  add_tap = conjure_eval_fn "(add-tap (requiring-resolve 'portal.api/submit))",
-  remove_tap = conjure_eval_fn "(remove-tap (requiring-resolve 'portal.api/submit))",
-
-  clear = conjure_eval_fn "(portal.api/clear)",
-
-  last_exception = conjure_eval_fn "(tap> (Throwable->map *e))",
-
+local conjure_taps = {
   tap_word = function()
     local word = conjure_word()
     conjure_eval("(tap> " .. word .. ")")
@@ -54,6 +53,31 @@ local portal_cmds = {
     local form = conjure_form(true)
     conjure_eval("(tap> " .. form .. ")")
   end,
+
+  last_exception = conjure_eval_fn "(tap> (Throwable->map *e))",
+}
+
+local portal_cmds = {
+  open = conjure_eval_fn [[
+    (def portal 
+      ((requiring-resolve 'portal.api/open)
+        {:theme :portal.colors/nord})) 
+    ]],
+
+  add_tap = conjure_eval_fn "(add-tap (requiring-resolve 'portal.api/submit))",
+  remove_tap = conjure_eval_fn "(remove-tap (requiring-resolve 'portal.api/submit))",
+  clear = conjure_eval_fn "(portal.api/clear)",
+  next_viewer = invoke_portal_command "(portal.ui.commands/select-next-viewer portal.ui.state/state)",
+  prev_viewer = invoke_portal_command "(portal.ui.commands/select-prev-viewer portal.ui.state/state)",
+  select_root = invoke_portal_command "(portal.ui.commands/select-root portal.ui.state/state)",
+  select_next = invoke_portal_command "(portal.ui.commands/select-next portal.ui.state/state)",
+  select_prev = invoke_portal_command "(portal.ui.commands/select-prev portal.ui.state/state)",
+  select_parent = invoke_portal_command "(portal.ui.commands/select-parent portal.ui.state/state)",
+  select_child = invoke_portal_command "(portal.ui.commands/select-child portal.ui.state/state)",
+  history_back = invoke_portal_command "(portal.ui.commands/history-back portal.ui.state/state)",
+  history_forward = invoke_portal_command "(portal.ui.commands/history-forward portal.ui.state/state)",
+  focus_selected = invoke_portal_command "(portal.ui.commands/focus-selected portal.ui.state/state)",
+  toggle_expand = invoke_portal_command "(portal.ui.commands/toggle-expand portal.ui.state/state)",
 }
 
 local portal_mappings = {
@@ -62,13 +86,17 @@ local portal_mappings = {
     o = { portal_cmds.open, "Portal open" },
     c = { portal_cmds.clear, "Portal clear" },
 
-    e = { portal_cmds.last_exception, "Portal last exception" },
-    w = { portal_cmds.tap_word, "Portal tap word" },
-    f = { portal_cmds.tap_form, "Portal tap form" },
-    r = { portal_cmds.tap_root_form, "Portal tap root form" },
+    e = { conjure_taps.last_exception, "Portal last exception" },
+    w = { conjure_taps.tap_word, "Portal tap word" },
+    f = { conjure_taps.tap_form, "Portal tap form" },
+    r = { conjure_taps.tap_root_form, "Portal tap root form" },
 
     R = { portal_cmds.remove_tap, "Remove tap"},
-    T = { portal_cmds.add_tap, "Add tap"}
+    T = { portal_cmds.add_tap, "Add tap"},
+    w = { portal_cmds.toggle_expand, "Toggle expand"},
+    x = { portal_cmds.select_root, "Select root"},
+    y = { portal_cmds.select_next, "Select next"},
+    z = { portal_cmds.select_prev, "Select prev"}
   }
 }
 
