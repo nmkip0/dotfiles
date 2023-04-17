a2d5e917a30f0cb1483e60ce4e7b3338e2f83af2
nmkip <juan.ignacio848@gmail.com>
2023-04-17 18:13:56 -0300

    conjure

--- nvim/lua/config/plugins/conjure/clojure.lua
+++ nvim/lua/config/plugins/conjure/clojure.lua
@@ -1,67 +1,62 @@
 local M = {}
 
 function M.config()
-	local extract = require("conjure.extract")
-	local eval = require("conjure.eval")
+  local extract = require("conjure.extract")
+  local eval = require("conjure.eval")
 
-	local function conjure_eval(form, passive)
-		return eval["eval-str"]({
-			code = form,
-			origin = "custom_command",
-			["passive?"] = passive,
-		})
-	end
+  local function conjure_eval(form, passive)
+    return eval["eval-str"]({
+      code = form,
+      origin = "custom_command",
+      ["passive?"] = passive,
+    })
+  end
 
-	local function conjure_eval_fn(form, passive)
-		return function()
-			conjure_eval(form, passive or false)
-		end
-	end
+  local function conjure_eval_fn(form, passive)
+    return function()
+      conjure_eval(form, passive or false)
+    end
+  end
 
-	local function conjure_eval_tap(form)
-		conjure_eval("(doto " .. form .. " tap>)")
-	end
+  local function conjure_eval_tap(form)
+    conjure_eval("(doto " .. form .. " tap>)")
+  end
 
-	local function conjure_word()
-		return extract.word().content
-	end
+  local function conjure_word()
+    return extract.word().content
+  end
 
-	local function conjure_form(is_root)
-		return extract.form({ ["root?"] = is_root }).content
-	end
+  local function conjure_form(is_root)
+    return extract.form({ ["root?"] = is_root }).content
+  end
 
-	local conjure_taps = {
-		eval_tap_word = function()
-			local word = conjure_word()
-			conjure_eval_tap(word)
-		end,
+  local conjure_taps = {
+    eval_tap_word = function()
+      local word = conjure_word()
+      conjure_eval_tap(word)
+    end,
 
-		eval_tap_form = function()
-			local form = conjure_form(false)
-			conjure_eval_tap(form)
-		end,
+    eval_tap_form = function()
+      local form = conjure_form(false)
+      conjure_eval_tap(form)
+    end,
 
-		eval_tap_root_form = function()
-			local form = conjure_form(true)
-			conjure_eval_tap(form)
-		end,
+    eval_tap_root_form = function()
+      local form = conjure_form(true)
+      conjure_eval_tap(form)
+    end,
 
-		tap_last_exception = conjure_eval_fn("(tap> (Throwable->map *e))"),
+    tap_last_exception = conjure_eval_fn("(tap> (Throwable->map *e))"),
 
-		tap_ns_publics = conjure_eval_fn("(tap> (ns-publics *ns*))"),
-	}
+    tap_ns_publics = conjure_eval_fn("(tap> (ns-publics *ns*))"),
+  }
 
-	return {
-		p = {
-			name = "+conjure",
-			e = { conjure_taps.eval_tap_word, "Eval word" },
-		},
-	}
+  return {
+    p = {
+      name = "+conjure",
+      e = { conjure_taps.eval_tap_word, "Eval word" },
+    },
+  }
 end
-
--- local config = require("conjure.config")
--- local client = require("conjure.client")
-
--- nvim.ex.function_(str.join("\n", {"ConjureEvalMotionOpFunc(kind)", "call luaeval(\"require('conjure.eval')['selection'](_A)\", a:kind)", "endfunction"}))
 
 return M
