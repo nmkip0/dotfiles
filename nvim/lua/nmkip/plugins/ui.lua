763a2edf99e83d35a76248abb8e3a4981c2bbef1
nmkip <juan.ignacio848@gmail.com>
2023-11-04 20:49:02 -0300

    bufferline for tabs

--- nvim/lua/nmkip/plugins/ui.lua
+++ nvim/lua/nmkip/plugins/ui.lua
@@ -64,6 +64,20 @@ return {
     end,
   },
   {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    version = "*",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      options = {
+        mode = "tabs",
+        themable = true,
+        separator_style = "thin",
+        always_show_bufferline = false,
+      },
+    },
+  },
+  {
     "nvim-lualine/lualine.nvim",
 
     event = "VeryLazy",
