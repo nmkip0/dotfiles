97fb74c275b4805b98561167628f5a8ef6e38dca
nmkip <juan.ignacio848@gmail.com>
2023-11-05 15:47:20 -0300

    git

---
+++ nvim/lua/nmkip/plugins/git.lua
@@ -0,0 +1,86 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPost" },
+    opts = {
+      signs = {
+        add = { text = "❙" },
+        change = { text = "❙" },
+        delete = { text = "" },
+        topdelete = { text = "" },
+        changedelete = { text = "❙" },
+        untracked = { text = "❙" },
+      },
+      on_attach = function(buffer)
+        local gs = require("gitsigns")
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+
+        -- stylua: ignore start
+        map("n", "]h", gs.next_hunk, "Next Hunk")
+        map("n", "[h", gs.prev_hunk, "Prev Hunk")
+        map({ "n", "v" }, "<leader>ghs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+        map({ "n", "v" }, "<leader>ghr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
+        map("n", "<leader>ghS", gs.stage_buffer, "Stage Buffer")
+        map("n", "<leader>ghu", gs.undo_stage_hunk, "Undo Stage Hunk")
+        map("n", "<leader>ghR", gs.reset_buffer, "Reset Buffer")
+        map("n", "<leader>ghp", gs.preview_hunk, "Preview Hunk")
+        map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
+        map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
+      end,
+    },
+  },
+  {
+    "NeogitOrg/neogit",
+    dependencies = {
+      "nvim-lua/plenary.nvim",         -- required
+      "nvim-telescope/telescope.nvim", -- optional
+      "sindrets/diffview.nvim",        -- optional
+      "ibhagwan/fzf-lua",              -- optional
+    },
+    keys = {
+      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Neogit" },
+    },
+    config = true
+  },
+  {
+    "sindrets/diffview.nvim",
+    keys = {
+      { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diff view" },
+      { "<leader>gf", "<cmd>DiffviewFileHistory %<cr>", desc = "File history" },
+      { "<leader>gl", "<cmd>DiffviewFileHistory<cr>", desc = "Git log" },
+      { "q", "<cmd>DiffviewClose<cr>", desc = "Close diff view" },
+    },
+    config = function()
+      local actions = require("diffview.actions")
+      require("diffview").setup({
+        enhanced_diff_hl = true,
+
+        keymaps = {
+          file_panel = {
+            {
+              "n",
+              "<Right>",
+              actions.select_entry,
+              { desc = "Open the diff for the selected entry" },
+            },
+            {
+              "n",
+              "<cr>",
+              actions.focus_entry,
+              { desc = "Focus the diff entry" },
+            },
+          },
+        },
+
+        hooks = {
+          diff_buf_win_enter = function()
+            vim.opt_local.foldenable = false
+          end,
+        },
+      })
+    end,
+  }
+}
