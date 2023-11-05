3f709c0eb4438bb80abc698e14a077402cdfebcd
nmkip <juan.ignacio848@gmail.com>
2023-11-05 15:45:50 -0300

    Window picker + neotree

--- nvim/lua/nmkip/plugins/editor.lua
+++ nvim/lua/nmkip/plugins/editor.lua
@@ -369,4 +369,49 @@ return {
     "NoahTheDuke/vim-just",
     ft = "just",
   },
+  {
+    's1n7ax/nvim-window-picker',
+    name = 'window-picker',
+    event = 'VeryLazy',
+    version = '2.*',
+    config = function()
+      require'window-picker'.setup({
+          autoselect_one = true,
+          highlights = {
+            statusline = {
+              focused = {
+                fg = '#ededed',
+                bg = '#e35e4f',
+                bold = true,
+              },
+              unfocused = {
+                bg = "#000000",
+                fg = "#FFE209",
+                bold = true,
+              },
+            },
+            winbar = {
+              focused = {
+                fg = '#ededed',
+                bg = '#e35e4f',
+                bold = true,
+              },
+              unfocused = {
+                fg = '#ededed',
+                bg = '#44cc41',
+                bold = true,
+              },
+            },
+          },
+          filter_rules = {
+            bo = {
+              filetype = { "neo-tree", "neo-tree-popup", "notify" },
+              buftype = { "terminal", "quickfix" },
+            },
+            file_name_contains = { "conjure-log-*" },
+          },
+          include_current = false,
+      })
+    end,
+  }
 }
