c667b8a8da7498109a0ccc02fdaf85a382fe802a
nmkip <juan.ignacio848@gmail.com>
2023-11-11 00:29:13 -0300

    Create workspace command

--- wezterm/keymap.lua
+++ wezterm/keymap.lua
@@ -35,6 +35,31 @@ function M.setup(config)
 
     -- Lastly, workspace
     { key = "w", mods = "LEADER",       action = act.ShowLauncherArgs { flags = "FUZZY|WORKSPACES" } },
+
+    {
+      key = 'W',
+      mods = 'CTRL|SHIFT',
+      action = act.PromptInputLine {
+        description = wezterm.format {
+          { Attribute = { Intensity = 'Bold' } },
+          { Foreground = { AnsiColor = 'Fuchsia' } },
+          { Text = 'Enter name for new workspace' },
+        },
+        action = wezterm.action_callback(function(window, pane, line)
+          -- line will be `nil` if they hit escape without entering anything
+          -- An empty string if they just hit enter
+          -- Or the actual line of text they wrote
+          if line then
+            window:perform_action(
+              act.SwitchToWorkspace {
+                name = line,
+              },
+              pane
+            )
+          end
+        end),
+      },
+    },
   }
 
   for i = 1, 9 do
